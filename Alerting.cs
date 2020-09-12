7b1394abb438ecdfdca6c930044e00936b5d611a
SoniaVadhnani <68679226+SoniaVadhnani@users.noreply.github.com>
2020-09-12 23:38:58 +0530

    Initial commit

---
+++ Alerting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitalsCSharp
+{
+     class Alerting
+    {
+         public interface IAlert
+        {
+
+             void SendAlert(string message);
+        }
+
+        public class AlertBySms : IAlert
+        {
+             public void SendAlert(string message)
+            {
+                Console.WriteLine($"SMS : {message}");
+            }
+        }
+
+        public class AlertBySound : IAlert
+        {
+             
+             public void SendAlert(string message)
+            {
+                Console.WriteLine($"Sound : {message}");
+            }
+        }
+
+    }
+}
---
+++ checker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mail;
+//using VitalsCSharp;
+
+namespace VitalsCSharp
+{
+    class Checker
+    {
+
+        /*static bool vitalsAreOk(float bpm, float spo2, float respRate)
+        {
+            if (bpm < 70 || bpm > 150)
+            {
+                return false;
+            }
+            else if (spo2 < 90)
+            {
+                return false;
+            }
+            else if (respRate < 30 || respRate > 95)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        */
+
+
+        #region source
+
+        public bool vitalsAreOk(Alerting.IAlert alert, string name, float value, float lower, float upper)
+        {
+            Vital vitalobj = new Vital(name, value, lower, upper);
+            if (!IsVitalLow(alert, vitalobj) && !IsVitalHigh(alert, vitalobj))
+                return true;
+
+            return false;
+        }
+
+        #endregion
+
+
+
+        #region target
+        public bool IsVitalLow(Alerting.IAlert alert,Vital vitalobject)
+        {
+            if (vitalobject.Value < vitalobject.Lower)
+            {
+                //call alert
+                alert.SendAlert($"The {vitalobject.Name} value is {vitalobject.Value} which is too LOW.");
+                return true;
+               
+            }
+
+            return false;
+        }
+
+        public bool IsVitalHigh(Alerting.IAlert alert,Vital vitalobject)
+        {
+            if (vitalobject.Value < vitalobject.Lower)
+            {
+                //call alert
+                alert.SendAlert($"The {vitalobject.Name} value is {vitalobject.Value} which is too HIGH.");
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+
+        static int Main()
+        {
+            
+
+            //alert is also passed with the name and val accordingly
+            Alerting.IAlert smsalert = new Alerting.AlertBySms();
+            Alerting.IAlert soundalert = new Alerting.AlertBySound();
+
+            Checker checkerobj1 = new Checker();
+            //checkerobj1.vitalsAreOk(smsalert, "spo2", 100, 95, 60);
+
+
+            VitalChecker.ExpectTrue(checkerobj1.vitalsAreOk(smsalert, "spo2", 98, 90, 101));
+            VitalChecker.ExpectFalse(checkerobj1.vitalsAreOk(smsalert, "spo2", 85, 90, 101));
+            // ExpectFalse(vitalsAreOk(40, 91, 92));
+            //Console.WriteLine("All ok");
+            return 0;
+        }
+    }
+}
